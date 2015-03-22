6bd1d4f9b42931be050e59307b5370e4d55cc14f
ckaufman21 <ckaufman21@users.noreply.github.com>
2015-03-22 12:27:36 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,14 @@
 # This function creates a special matrix object that can cache its inverse (assuming the matrix supplied is invertible).
 
-makeCacheMatrix <- function(x = matrix()) {.
+makeCacheMatrix <- function(x = matrix()) {
     i <- NULL                                   # Initialize 'i' variable to NULL so the function can run the first time through.
     set <- function(y) {                        # Function 'set' reads in argument passed to makeCacheMatrix.
         x <<- y                                 # Set 'x' for the parent (set) environment equal to 'y'.
         i <<- NULL                              # Set 'i' for the parent (makeCacheMatrix) environment equal to NULL.
     }
     get <- function() x                         # Function 'get' assigns the matrix 'x' to it.
-    setinv <- function(inverse) i <<- inverse   # Function 'setinv' takes a value and sets it to the value of 'i' in the.
-    getinv <- function() i                      # makeCacheMatrix environment. Function 'getinv' returns the value of 'i'.
+    setinv <- function(inverse) i <<- inverse   # Function 'setinv' takes a value ('inverse') and sets it to the value of 'i'
+    getinv <- function() i                      # in the makeCacheMatrix environment. Function 'getinv' returns the value of 'i'.
     list(set = set, get = get,                  # List the values of the functions in makeCacheMatrix.
     setinv = setinv,
     getinv = getinv)
@@ -25,7 +25,7 @@ cacheSolve <- function(x, ...) {
         return(i)
     }
     data <- x$get()                             # If the 'x' environment has never been evaluated, pull the x matrix into a local
-    i <- solve(data, ...)                       # variable ('data'). Calculate the inverse of the matrix 'x' by calling the 'solve'
-    x$setinv(i)                                 # function. Assign the calculated inverse to the 'x' environment using 'setmean'
+    i <- solve(data, ...)                       # variable ('data'). Calculate the inverse of the matrix 'x' by calling the 'solve'  
+    x$setinv(i)                                 # function. Assign the calculated inverse to the 'x' environment using 'setinv'
     i                                           # Print the calculated inverse.
 }
